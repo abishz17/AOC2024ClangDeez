8c52af9d565aa5b0291b57310c3d96544f5655ce
abishz17 <075bct008.abish@pcampus.edu.np>
2024-12-04 00:25:10 +0545

    day1 part1

---
+++ day1/day1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *elem1, const void *elem2) {
+  int f = *((int *)elem1);
+  int s = *((int *)elem2);
+  if (f > s)
+    return 1;
+  if (f < s)
+    return -1;
+  return 0;
+}
+
+int main() {
+  FILE *file = fopen("day1.txt", "r");
+  if (file == NULL) {
+    printf("Unable to open file");
+    return 1;
+  }
+  int left[1000];
+  int right[1000];
+  int diffarr[1000];
+  int result;
+  int count = 0;
+  while (fscanf(file, "%d %d", &left[count], &right[count]) == 2) {
+    count++;
+  }
+  qsort(left, sizeof(left) / sizeof(*left), sizeof(*left), compare); 
+  qsort(right, sizeof(right) / sizeof(*right), sizeof(*right), compare);
+  for (int i = 0; i < 1000; i++) {
+    diffarr[i] = abs(left[i] - right[i]);
+  }
+  for(int i = 0 ; i < 1000; i++){
+    // printf("%d\n", diffarr[i]);
+    result+=diffarr[i];
+  }
+  printf("The final result is: %d ",result);
+  fclose(file);
+}
