1521bc3a51195da1118f4773f18c79212c15ec37
abishz17 <075bct008.abish@pcampus.edu.np>
2024-12-04 00:44:59 +0545

    day1 part 2

--- day1/day1.c
+++ day1/day1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int compare(const void *elem1, const void *elem2) {
-  int f = *((int *)elem1);
+  int f = *((int *)elem1); // Casting void pointer to integer pointer
   int s = *((int *)elem2);
   if (f > s)
     return 1;
@@ -20,17 +20,32 @@ int main() {
   int left[1000];
   int right[1000];
   int diffarr[1000];
+  int occurencearr[1000];
   int result;
+  int similarityScore;
   int count = 0;
   while (fscanf(file, "%d %d", &left[count], &right[count]) == 2) {
     count++;
   }
-  qsort(left, sizeof(left) / sizeof(*left), sizeof(*left), compare); 
+  qsort(left, sizeof(left) / sizeof(*left), sizeof(*left), compare);
   qsort(right, sizeof(right) / sizeof(*right), sizeof(*right), compare);
   for (int i = 0; i < 1000; i++) {
     diffarr[i] = abs(left[i] - right[i]);
-    result+=diffarr[i];
+    result += diffarr[i];
   }
-  printf("The final result is: %d ",result);
+  for (int i = 0; i < 1000; i++) {
+    int similarityCount = 0;
+    for (int j = 0; j < 1000; j++) {
+      if (left[i] == right[j]) {
+        similarityCount++;
+      }
+    }
+    occurencearr[i] = left[i] * similarityCount;
+  }
+  for(int i = 0 ; i < 1000 ; i++){
+    similarityScore += occurencearr[i];
+  }
+  printf("The final result is: %d ", result);
+  printf("The similarity score is: %d ", similarityScore);
   fclose(file);
 }
